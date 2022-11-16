e04ca25794ccd2c20e664bcec4bab1bab6184b9b
Firex86 <46250017+Firex86@users.noreply.github.com>
2022-11-16 21:38:10 +0200

    Update toiminnot.js

--- toiminnot.js
+++ toiminnot.js
@@ -8,10 +8,32 @@ function loadXMLFile() {
     xmlhttp.open("GET", "https://www.finnkino.fi/xml/Schedule/", true);
     xmlhttp.send();
 
+    if(xmlhttp.readyState == 1) {
+        document.getElementById("Sisältö").innerHTML = "Lataus käynnissä...";
+    }
+
     xmlhttp.onreadystatechange = function() {
         if(xmlhttp.readyState == 4 && xmlhttp.status == 200) {
         document.getElementById("Sisältö").innerHTML = xmlhttp.responseText;
+        document.getElementById("Sisältö").innerHTML = txt;
+
+
+        var xmlDoc = xmlhttp.responseXML;
+
+        var ElokuvaNimi = xmlDoc.getElementsByTagName("Title");
+        var Teatteri = xmlDoc.getElementsByTagName("Theatre");
         }
+
+        var txt = "<table>";
+
+        for(i = 0; i < ElokuvaNimi.length; i++) {
+            txt += "<tr><td>" + ElokuvaNimi[i].childNodes[0].nodeValue + "</td>"+
+                    "<td>"    + Teatteri[i].childNodes[0].nodeValue +  "</td></tr>";
+            console.log(txt);
+        }
+        txt += "</table>";
+
+        document.getElementById("taulukko").innerHTML = txt;
        
     }
 }
