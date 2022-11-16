d6b904a233e026974aa1eabc318702da86579e28
Firex86 <46250017+Firex86@users.noreply.github.com>
2022-11-16 15:28:35 +0200

    Update toiminnot.js

--- toiminnot.js
+++ toiminnot.js
@@ -1,1 +1,12 @@
+function loadXMLFile() {
+    var xmlhttp = new XMLHttpRequest();
 
+    xmlhttp.open("GET", "https://www.finnkino.fi/xml/Schedule/", true);
+    xmlhttp.send();
+
+    xmlhttp.onreadystatechange = function() {
+        
+        document.getElementById("Sisältö").innerHTML = xmlhttp.responseText;
+       
+    }
+}
